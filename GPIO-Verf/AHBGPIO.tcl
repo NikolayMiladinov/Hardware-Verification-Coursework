ef8d745195cf1919eeb2f1b9bf5a86eb528cde30
NikolayMiladinov <nikolaj.miladinov@gmail.com>
2022-12-12 08:56:12 +0000

    Added tcl file for GPIO for JasperGold

---
+++ GPIO-Verf/AHBGPIO.tcl
@@ -0,0 +1,13 @@
+clear -all
+analyze -clear
+analyze -sv rtl/AHB_GPIO/AHBGPIO.sv
+elaborate -bbox_gpio 64 -top AHBGPIO
+
+clock HCLK
+reset -expression !(HRESETn)
+
+task -set <embedded>
+set_proofgrid_max_jobs 4
+set_proofgrid_max_local_jobs 4
+
+# cover -name test_cover_from_tcl {@(posedge HCLK) disable iff (HRESETn) done }
